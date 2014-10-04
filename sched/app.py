de2d50d76c6426fcd8022fc184b130a1cec40e72
sustaitangel <luis.sustaita@cimat.mx>
2014-10-04 17:15:18 -0500

    agrgar URL

--- sched/app.py
+++ sched/app.py
@@ -2,6 +2,29 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-   return 'Hello, world u Hola maldito mundo!!'
+   return 'Hello, world i hate you!!'
 if __name__ == '__main__':
    app.run()
+
+@app.route('/appointments/')
+def appointment_list():
+   return 'Listing of all appointments we have.'
+@app.route('/appointments/<int:appointment_id>/')
+def appointment_detail(appointment_id):
+   return 'Detail of appointment #{}.'.format(appointment_id)
+@app.route(
+   '/appointments/<int:appointment_id>/edit/', methods=['GET', 'POST'])
+#@app.route(1) and def appointment_edit(...).
+def appointment_edit(appointment_id):
+   return 'Form to edit appointment #.'.format(appointment_id)
+@app.route(
+   '/appointments/create/',
+   methods=['GET', 'POST'])
+def appointment_create():
+   return 'Form to create a new appointment.'
+@app.route(
+   '/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
+def appointment_delete(appointment_id):
+   raise NotImplementedError('DELETE')
+
+
