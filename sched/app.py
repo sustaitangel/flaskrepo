eac8d4c0d603fb2ab824b5fbcde1477ff3469d7e
mariamota <juanis315@hotmail.com>
2014-10-07 16:56:33 -0500

    Archivos.py

--- sched/app.py
+++ sched/app.py
@@ -13,51 +13,53 @@ db.Model = Base
 
 @app.route('/')
 def hello():
-"""
->>> hello()
-'Hello, world, i hate you!!!'
-"""
-return 'Hello, world, i hate you!!!'
+    """
+    >>> hello()
+    'Hello, world, i hate you!!!'
+    """
+    return 'Hello, world, i hate you!!!'
 
 
 @app.route('/appointments/')
 def appointment_list():
-"""
->>> appointment_list()
-'Listing of all appointments we have.'
-"""
-return 'Listing of all appointments we have.'
+    """
+    >>> appointment_list()
+    'Listing of all appointments we have.'
+    """
+    return 'Listing of all appointments we have.'
 
 
 @app.route('/appointments/<int:appointment_id>/')
 def appointment_detail(appointment_id):
-"""
->>> appointment_detail(3)
-'Detail of appointment #3.'
-"""
-return 'Detail of appointment #{}.'.format(appointment_id)
+    """
+    >>> appointment_detail(3)
+    'Detail of appointment #3.'
+    """
+    return 'Detail of appointment #{}.'.format(appointment_id)
 
 
 @app.route('/appointments/<int:appointment_id>/edit/', methods=['GET', 'POST'])
 def appointment_edit(appointment_id):
-"""
->>> appointment_edit(5)
-'Form to edit appointment #5.'
-"""
-return 'Form to edit appointment #{}.'.format(appointment_id)
+    """
+    >>> appointment_edit(5)
+    'Form to edit appointment #5.'
+    """
+    return 'Form to edit appointment #{}.'.format(appointment_id)
 
 
 @app.route('/appointments/create/', methods=['GET', 'POST'])
 def appointment_create():
-"""
->>> appointment_create()
-'Form to create a new appointment.'
-"""
-return 'Form to create a new appointment.'
+    """
+    >>> appointment_create()
+    'Form to create a new appointment.'
+    """
+    return 'Form to create a new appointment.'
 
 
 @app.route('/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
 def appointment_delete(appointment_id):
-raise NotImplementedError('DELETE')
+    raise NotImplementedError('DELETE')
+
+
 if __name__ == '__main__':  # pragma: no cover
-app.run(debug=True)
+    app.run(debug=True)
