2372f7bfe3f2d42d2365ba7c177bd5ec6c56376d
mariamota <juanis315@hotmail.com>
2014-10-11 11:14:37 -0500

    PruebasUnitarias

---
+++ PU.py
@@ -0,0 +1,88 @@
+import unittest
+from sched import models
+from sched import forms
+from sched import filters
+from datetime import datetime
+from datetime import timedelta
+
+
+class pruebaFilters(unittest.TestCase):
+
+    def testDo_datetimeNone(self):
+        fecha = filters.do_datetime(None)
+        self.assertEqual(fecha, '')
+
+    def testDo_dateNone(self):
+        fecha = filters.do_date(None)
+        self.assertEqual(fecha, '')
+
+    def testDo_datetime(self):
+        fecha = filters.do_datetime(datetime(1990, 07, 01, 07, 06, 00))
+        self.assertEqual(fecha, '1990-07-01 - Sunday at 7:06am')
+        self.assertNotEqual(fecha, '1990-07-01')
+
+    def testDo_date(self):
+        fecha = filters.do_date(datetime(1990, 07, 01, 07, 06, 00))
+        self.assertEqual(fecha, '1990-07-01 - Sunday')
+        self.assertNotEqual(fecha, '1990-07-01 - Sunday at 7:06am')
+
+    def testDo_duration(self):
+        d = filters.do_duration(259578)
+        self.assertEqual(d, '3 days, 6 minutes, 18 seconds')
+
+
+class pruebaForm(unittest.TestCase):
+
+    def testForm(self):
+        f = forms.AppointmentForm()
+        self.assertEqual(
+            '<input id="title" name="title" type="text" value="">',
+            str(f.title))
+        self.assertEqual(
+            '<input id="start" name="start" type="text" value="">',
+            str(f.start))
+        self.assertEqual(
+            '<input id="end" name="end" type="text" value="">',
+            str(f.end))
+        self.assertEqual(
+            '<input id="allday" name="allday" type="checkbox" value="y">',
+            str(f.allday))
+        self.assertEqual(
+            '<input id="location" name="location" type="text" value="">',
+            str(f.location))
+        self.assertEqual(
+            '<textarea id="description" name="description"></textarea>',
+            str(f.description))
+
+    def testLogIn(self):
+        f = forms.LoginForm()
+        self.assertEqual(
+            '<input id="username" name="username" type="text" value="">',
+            str(f.username))
+        self.assertEqual(
+            '<input id="password" name="password" type="password" value="">',
+            str(f.password))
+
+
+class pruebaApponintment(unittest.TestCase):
+
+    def testDurationAppointment(self):
+        now = datetime.now()
+        a = models.Appointment(id=1, title='Past Meeting',
+                               start=now - timedelta(days=3, seconds=3600),
+                               end=now - timedelta(days=3),
+                               allday=False,
+                               location='The Office')
+        self.assertEqual(3600, a.duration)
+
+    def testReprAppointment(self):
+        now = datetime.now()
+        a = models.Appointment(id=1, title='Important Meeting',
+                               start=now + timedelta(days=3),
+                               end=now + timedelta(days=3, seconds=3600),
+                               allday=False, location='The Office')
+        self.assertEqual('<Appointment: 1>', a.__repr__())
+
+
+if __name__ == '__main__':
+    unittest.main()
